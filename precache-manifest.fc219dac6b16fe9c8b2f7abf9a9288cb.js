c49a1eff5cde1efda24d3fbb1694f3782abce93f
Jason Carney <jcarney@fandom.com>
2019-10-24 14:51:08 -0500

    Updates

---
+++ precache-manifest.fc219dac6b16fe9c8b2f7abf9a9288cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "88af228f37c262817ee3",
+    "url": "/static/js/main.88af228f.chunk.js"
+  },
+  {
+    "revision": "285d452b8e60ad6320af",
+    "url": "/static/js/1.285d452b.chunk.js"
+  },
+  {
+    "revision": "285d452b8e60ad6320af",
+    "url": "/static/css/1.0c6b1f87.chunk.css"
+  },
+  {
+    "revision": "00f0b7aaa70f2b7804e45c31fc1e91f4",
+    "url": "/index.html"
+  }
+];
